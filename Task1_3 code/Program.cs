fcdf24ae327efe1a1a861f1d24cc51ae71064980
Rahma Sabry <rahmasabry654@gmail.com>
2025-07-21 19:43:45 +0300

    Uploading Third task

---
+++ Task1_3 code/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1_3_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i < n)
+                        Console.Write(i + " ,");
+                    else
+                        Console.Write(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
